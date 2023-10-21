441d1aed65d4016764a5ed8db0364432c8bfdada
Akabim <abimanyu.ari.d.123@gmail.com>
2023-10-21 11:19:18 +0700

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       colors: {
         primary : "#FF3497"
-      }
+      },
+      boxShadow: {
+        black: '0 0 0 2px rgba(0, 0, 0, 1)',
+      },
     },
   },
   plugins: [],
